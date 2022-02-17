4d79fbc6a2f2ebc4122590f7bb264ef0e700b3bf
JamesFahey <james.fahey@hotmail.co.uk>
2022-02-17 20:06:51 +0000

    add actions to update status in admin url

--- events/admin.py
+++ events/admin.py
@@ -6,5 +6,11 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
 
+    list_display = ('name', 'event_type', 'status', 'event_date')
+    search_fields = ['name', 'event_type']
     list_filter = ('status', 'event_date')
     summernote_fields = ('info')
+    actions = ['update_status']
+
+    def update_status(self, request, queryset):
+        queryset.update(status=1)
