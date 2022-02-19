86fc7d5c09643736837a12abea11202c4fc56d74
JamesFahey <james.fahey@hotmail.co.uk>
2022-02-19 10:28:28 +0000

    add event_page html and link views and url.py

--- events/views.py
+++ events/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Event
 
 def home(request):
     return render(request, 'index.html')
+
+
+class EventList(generic.ListView):
+    model = Event
+    queryset = Event.objects.order_by("event_date")
+    template_name = "event_page.html"
--- scrawndog_media/urls.py
+++ scrawndog_media/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from events.views import home
+from events import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home, name="home"),
     path('summernote/', include('django_summernote.urls')),
+    path('event_page/', views.EventList.as_view(), name='Event Page')
 ]
