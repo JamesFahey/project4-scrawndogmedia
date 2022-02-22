f3ac45c87b28b7643c45738e0f9b962c93979992
JamesFahey <james.fahey@hotmail.co.uk>
2022-02-22 11:08:15 +0000

    deploy to heroku

--- events/forms.py
+++ events/forms.py
@@ -1,18 +1,33 @@
-from django import forms
 from .models import Event
+from django import forms
 
-EventType = (
-    ("WEDDING", "Wedding"),
-    ("CHRISTENING", "Christening"),
-    ("OTHER", "Other"),
-)
 
-class BookingForm(forms.Form):
-    name = forms.CharField(label="Full Name", max_length=100)
-    email = forms.EmailField(label="Email")
-    event_type = forms.ChoiceField(choices=EventType)
-    event_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
-    info = forms.CharField(label="Please tell us as much as you can about your day", max_length=500)
+# EventType = (
+#     ("WEDDING", "Wedding"),
+#     ("CHRISTENING", "Christening"),
+#     ("OTHER", "Other"),
+# )
+
+# class BookingForm(forms.Form):
+#     name = forms.CharField(label="Full Name", max_length=100)
+#     email = forms.EmailField(label="Email")
+#     event_type = forms.ChoiceField(choices=EventType)
+#     event_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+#     info = forms.CharField(label="Please tell us as much as you can about your day", max_length=500)
     
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ('name',
+            'email',
+            'event_type',
+            'event_date',
+            'info',)
+        widgets = {
+            'event_date': forms.DateInput(format=('%m/%d/%Y'), attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
+        }
+        labels = {
+                'info': ('Tell us as much as you can about your day:'),
+            }
 
 
--- scrawndog_media/settings.py
+++ scrawndog_media/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["scrawndogmedia.herokuapp.com", "localhost"]
 
@@ -50,6 +52,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'django_summernote',
     'events',
 ]
@@ -58,6 +61,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL ='/'
 LOGOUT_REDIRECT_URL ='/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
